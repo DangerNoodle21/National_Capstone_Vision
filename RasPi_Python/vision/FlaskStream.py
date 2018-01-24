a12082e34d5af1334049412ea00a5cd1e2c1eda4
DangerNoodle21 <DangerNoodle21@users.noreply.github.com>
2018-01-24 13:56:41 -0800

    Updating Python

---
+++ RasPi_Python/vision/FlaskStream.py
@@ -0,0 +1,29 @@
+'''
+Created on Jan 21, 2018
+
+@author: marcd
+
+'''
+
+from vision import cameraClass
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(cameraClass()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
