a8138720097b43e166d5e98d9880d7861c2cb4c9
Green-Shark <Green-Shark@users.noreply.github.com>
2018-01-25 19:21:05 -0800

    Stuff

---
+++ ArduinoLibrary1/ArduinoLibrary1/ArduinoLibrary1.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		ArduinoLibrary1.cpp
+ Created:	1/25/2018 7:06:11 PM
+ Author:	marcd
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "ArduinoLibrary1.h"
+
+
---
+++ ArduinoLibrary1/ArduinoLibrary1/ArduinoLibrary1.h
@@ -0,0 +1,19 @@
+/*
+ Name:		ArduinoLibrary1.h
+ Created:	1/25/2018 7:06:11 PM
+ Author:	marcd
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _ArduinoLibrary1_h
+#define _ArduinoLibrary1_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
