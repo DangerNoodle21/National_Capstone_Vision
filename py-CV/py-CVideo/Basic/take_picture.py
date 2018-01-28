b51a43b953dee251d24e1023cf73d8277b7c2ad3
Green-Shark <Green-Shark@users.noreply.github.com>
2018-01-27 20:05:18 -0800

    Fixed and Combined Scripts

--- py-CV/py-CVideo/Basic/take_picture.py
+++ py-CV/py-CVideo/Basic/take_picture.py
@@ -1,5 +1,11 @@
 
 """
+In this code:
+
+1. Takes picture with keyboard press
+2. Writes Words picture takes
+3. Clicking Puts a circle on the Screen
+
 
 Press 'p' to take a picture from stream
 
@@ -15,24 +21,57 @@ import numpy as np
 cap = cv2.VideoCapture(0) # Video Camera Scource - 0
 
 def keyboard():
+
+    #Keyboard Function
+
+    #Reads Video, names capture picture_1, outputs to console
     ret, picture_1 = cap.read()
     print("Pressed")
-    cv2.namedWindow("Picture")
+
+    #Creates Named window, Resizes the pictures, add text, Shows picture
+    cv2.namedWindow("Picture_1")
     picture_1 = cv2.resize(picture_1, (0,0), fx=1, fy=1)
-    cv2.imshow("Picture", picture_1)
 
+    #Adds Text to picture
+    cv2.putText(picture_1, 'ADD TEXT', (210, 240), font, 4, (0,255,0), 4, cv2.LINE_AA)
+    cv2.imshow("Picture_1", picture_1)
 
+#Circle Parameters for click function
+color = (0,255,0)
+line_width = 3
+radius = 100
+point = (0,0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Function Click
+def click(event, x, y, flags, param):
+    #Global variable 
+    global point, pressed
+
+    #Event Handler for click, left mouse button down
+    if event == cv2.EVENT_LBUTTONDOWN:
+        
+        #Prints to console, then changed Point Coordinates
+        print("Pressed", x, y)
+        point = (x, y)
+
+# Named Window and Call backing event handler for mouse click
 cv2.namedWindow("Video")
-
+cv2.setMouseCallback("Video", click)
 
 #Video Loop
 while(True):
-
+     
     #video Capture
     ret, frame = cap.read()
-
+        
     #Resize Frame
     frame = cv2.resize(frame, (0,0), fx=1.5, fy=1.5)
+
+    #Draw Circle Function
+    cv2.circle(frame, point, radius, color, line_width)
+
+    #
     cv2.imshow("Video", frame)
 
 
---
+++ py-CV/py-CVideo/Video/video_user_thresh.py
@@ -0,0 +1,94 @@
+"""
+In this code:
+
+1. Takes picture with keyboard press
+2. Writes Words picture takes
+3. Clicking Puts a circle on the Screen
+
+
+
+Press 'q' to quit
+
+
+"""
+
+
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0) # Video Scource
+
+cv2.namedWindow("Video")
+
+
+while(True):
+
+    #Pulls image from stream
+    ret, takePic1 = cap.read()
+    
+    #Converts image from R,G,b to Gray Scale
+    takePic1 = cv2.cvtColor(takePic1, cv2.COLOR_RGB2GRAY)
+
+    #Bluring the image, for easier ID, Gaussian Blur
+    takePic1 = cv2.GaussianBlur(takePic1, (3,3),0)
+
+    #User Threshold 
+    thresh = 160
+    ret, user_thresh = cv2.threshold(takePic1, thresh, 255, cv2.THRESH_BINARY)
+
+    #Finding contours, writing to object 'contours'
+    _, contours, hierarchy = cv2.findContours(user_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    print(len(contours))
+
+    #Array for holding filtered contours
+    filtered = []
+    
+
+    #For loop for filtering out contours based on pixel Area
+    for c in contours:
+        if cv2.contourArea(c) < 1000:
+            continue
+        elif cv2.contourArea(c) > 3000:
+            continue
+        else:
+            filtered.append(c)
+    print(len(filtered))
+
+    objects = np.zeros([takePic1.shape[0], takePic1.shape[1],3], 'uint8')
+
+
+    for c in filtered:
+               
+        #Draws and outline around the contour, green
+        cv2.drawContours(objects, [c], -1, (0,255,0), 3)
+
+        
+        #Centriod
+        # Had to add +1 to advoid Dividion by Zero erro
+        M = cv2.moments(c)
+        cx = int( M['m10']/(M['m00'] + 1))
+        cy = int( M['m01']/(M['m00'] + 1))
+
+        #Centriods
+        cv2.circle(objects, (cx,cy), 4, (0, 0, 255), -1)
+        #Area and Perimter
+        area = cv2.contourArea(c)
+        perimeter = cv2.arcLength(c, True)
+
+        print("(X,Y): ", cx, cy, "Area: ", area, "Perimter: ", perimeter)
+    
+    cv2.imshow("Video", user_thresh)
+
+
+    cv2.imshow("Objects", objects)
+
+
+    #Picture / Break if statement
+    ch2 = cv2.waitKey(1)
+    if ch2 & 0xFf == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
