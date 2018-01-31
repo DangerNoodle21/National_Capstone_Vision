a924aaf710c887ea71d1e69a626a8c4dc1965295
Green-Shark <Green-Shark@users.noreply.github.com>
2018-01-31 15:59:09 -0800

    a

--- py-CV/py-CVideo/Video/vid_Pi-Ard_Contours.py
+++ py-CV/py-CVideo/Video/vid_Pi-Ard_Contours.py
@@ -111,6 +111,7 @@ def videoStream():
             
             for i in i2cData:
                 write_I2C(int(ord(i)))
+                time.sleep(.001)
             write_I2C(int(0x0A))
 
         cv2.imshow("Video", canny_video)
