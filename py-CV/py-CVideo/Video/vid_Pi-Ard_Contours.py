8fbd9a577412a5240658680e7be94a02b147c3c7
Green-Shark <Green-Shark@users.noreply.github.com>
2018-01-31 15:20:01 -0800

    a

--- py-CV/py-CVideo/Video/vid_Pi-Ard_Contours.py
+++ py-CV/py-CVideo/Video/vid_Pi-Ard_Contours.py
@@ -101,7 +101,7 @@ def videoStream():
 
             print("(X,Y): ", cx, cy, "Permeter:", perimeter, "Area:", area)
 
-            i2cData.extend([cx, cy, perimeter, area])
+            i2cData.extend([str(cx), str(cy), str(perimeter), str(area)])
 
             for i in i2cData:
                 write_I2C(int(ord(i)))
