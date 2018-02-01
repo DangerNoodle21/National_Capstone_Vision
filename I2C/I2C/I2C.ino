16155a518b31a6ec1ac2e85047c31d9fb748256f
Green-Shark <Green-Shark@users.noreply.github.com>
2018-01-31 20:50:04 -0800

    Changes

---
+++ I2C/I2C/I2C.ino
@@ -0,0 +1,40 @@
+/*
+ Name:		I2C.ino
+ Created:	1/31/2018 8:48:24 PM
+ Author:	marcd
+*/
+
+
+#include <Wire.h>
+
+#define i2c_ADDRESS 0x04
+
+char input_numbers[1000];
+
+void receiveData(int byteCount) {
+	int i = 0;
+	while (Wire.available()) {
+		input_numbers[i] = Wire.read();
+		i++;
+	}
+	input_numbers[i] = '\0';
+	Serial.print(input_numbers);
+}  // end while
+
+
+void setup()
+{
+	pinMode(LED_BUILTIN, OUTPUT);
+
+
+	Serial.begin(9600);
+	Wire.begin(i2c_ADDRESS);
+	// define callbacks for i2c communication
+	Wire.onReceive(receiveData);
+	//  Wire.onRequest(sendData);
+}
+
+void loop()
+{
+	delay(100);
+}
