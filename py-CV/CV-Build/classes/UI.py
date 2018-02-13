fbc5b538ea3e4fe351c6c90714e57228ace45f0b
Green-Shark <Green-Shark@users.noreply.github.com>
2018-02-12 16:48:29 -0800

    1

--- py-CV/CV-Build/classes/UI.py
+++ py-CV/CV-Build/classes/UI.py
@@ -1,55 +1,100 @@
 import curses
-
+import os
+from colorclass import Color, Windows
+from terminaltables import SingleTable
+from colorclass import Color, Windows
+from terminaltables import SingleTable
 
 class userInteraction(object):
 
-    def user_Camera(stdscr):
-        print("What camera? 1 = USB, 0 = Built-in")
-        userAnswer = stdscr.getch()
-        if userAnswer == 49:
-            userCamera = '{autocyan}USB{/autocyan}'
-        else:
-            userCamera = '{autocyan}BUILT-IN{/autocyan}'
+    Windows.enable(auto_colors=True, reset_atexit=True)
+    begin_x = 35; begin_y = 0; height = 4; width = 30
 
-        return userCamera
+    userConsole = '{autocyan}No{/autocyan}'
+    userObject = '{autocyan}Pratice Square{/autocyan}'
+    userCamera = '{autocyan}BUILT-IN{/autocyan}'
+
+    def cls():
+        os.system('cls' if os.name=='nt' else 'clear')
+
+
+    def user_Camera(stdscr):
+        print("What camera? (1 = USB, 0 = Built-in): ")
+        userAnswer = stdscr.getch()
+
+        if userAnswer == 49:
+            userInteraction.userCamera = '{autocyan}USB{/autocyan}'
+            print(1)
+        else:
+            userInteraction.userCamera = '{autocyan}BUILT-IN{/autocyan}'
+            print(0)
 
 
     def user_Object(stdscr):
         print("What objct? 1 = Power Cube, 0 = Pratice Square")
         userAnswer = stdscr.getch()
-        if userAnswer == 49:
-            userObject = '{autocyan}Power Cube{/autocyan}'
+  
+        if userAnswer == 1:
+            userInteraction.userObject = '{autocyan}Power Cube{/autocyan}'
+            print(1)
         else:
-            userObject = '{autocyan}Pratice Square{/autocyan}'
+            userInteraction.userObject = '{autocyan}Pratice Square{/autocyan}'
+            print(0)
 
 
     def user_Console(stdscr):
         print("Enable Dynamic Console? 1 = Yes, 0 = No")
         userAnswer = stdscr.getch()
-        if userAnswer == 49:
-            userConsole = '{autocyan}Yes{/autocyan}'
+
+        if userAnswer == 1:
+            userInteraction.userConsole = '{autocyan}Yes{/autocyan}'
+            print(1)
         else:
-            userConsole = '{autocyan}No{/autocyan}'
+            userInteraction.userConsole = '{autocyan}No{/autocyan}'
+            print(0)
 
     def askCapture(stdscr):
+        #Clears and resents
+        stdscr.clear(); stdscr.refresh(); 
+        #Turns on Keypad
+        stdscr.keypad(True); curses.start_color()
+
+
+        # Title Talbe
+        cvTitle = SingleTable([['Computer Vision Program']])
+
+        #User Choices
+        choiceArray = [
+            [Color('Camera'), Color(userInteraction.userCamera)],
+            [Color('Target'), Color(userInteraction.userObject)],
+            [Color('Console-Out'), Color(userInteraction.userConsole)]        
+        ]
+
+        userChoiceTable = SingleTable(choiceArray)
+        userChoiceTable.inner_heading_row_border = False
+        userChoiceTable.inner_row_border = True
+        userChoiceTable.justify_columns = {0: 'center', 1: 'center', 2: 'center'}
+
+        print(cvTitle.table)
+
+        #Geting Operational Specifics
+        userInteraction.user_Camera(stdscr)
+        userInteraction.user_Object(stdscr)
+        userInteraction.user_Console(stdscr)
+
+        #Clear Screen
+        userInteraction.cls()
 
         stdscr = curses.initscr()
+        #Repriting Screen
+        print(cvTitle.table)
+        print(userChoiceTable.table)
 
-        stdscr.clear()
-        stdscr.refresh()
-        
-        stdscr.keypad(True)
-        curses.start_color()
+      
 
- 
-        begin_x = 20; begin_y = 7; height = 5; width = 40
-
-        win_title = curses.newwin(height, width, begin_y, begin_x)
+        win_title = curses.newwin(userInteraction.height, userInteraction.width, userInteraction.begin_y, userInteraction.begin_x)
         win_title.box(0,0)
 
-
-
-        stdscr.refresh()
         win_title.refresh()
 
         counter = 0
